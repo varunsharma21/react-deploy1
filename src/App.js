14f260a637635080a89102d1c5fcb4eb0e7ea891
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2021-10-12 00:12:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My first react deployment</h1>
+      <h1>My first react deployment!</h1>
       <h2>Author: Varun Sharma</h2>
     </div>
   );
